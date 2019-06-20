2b15c9d07c9f91929740948e0a98717d9f16f23c
mobile-bungalow <pwmay@ucsc.edu>
2019-06-20 14:16:55 -0700

    checkpoint before texture

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,35 @@
+mod bird;
+mod pipe;
+mod render_pl;
+extern crate piston;
+
+// piston reqs
+use piston_window::*;
+// user input
+use input::*;
+use piston::event_loop::{EventLoop, EventSettings, Events};
+
+
+use vecmath::Vector2;
 
 fn main() {
-    println!("Hello, world!");
+    // build window
+    let opengl = OpenGL::V3_2;
+    let mut window: PistonWindow = WindowSettings::new("flappy bird", (950, 800))
+        .title("Not Flappybird".to_string())
+        .graphics_api(opengl)
+        .build()
+        .unwrap_or_else(|e| panic!("Window didn't build: {}", e));
+
+
+    // intialize entities
+
+    // event loop
+
+    let mut events = Events::new(EventSettings::new().ups(60).max_fps(60));
+
+
+    while let Some(ev) = events.next(&mut window) {
+        if let Some(_) = ev.update_args() {}
+    }
 }
