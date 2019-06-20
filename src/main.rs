5eb889cc6560b10d0e62399fe57e01bc8624015e
mobile-bungalow <pwmay@ucsc.edu>
2019-06-20 16:40:15 -0700

    bg works, pipes

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,19 @@ extern crate piston;
 // piston reqs
 use piston_window::*;
 // user input
-use input::*;
+use graphics::rectangle::square;
 use piston::event_loop::{EventLoop, EventSettings, Events};
 
 
 use vecmath::Vector2;
 
+// width height pair
+static WINDIMS: [f64; 2] = [700.0, 350.0];
+
 fn main() {
     // build window
     let opengl = OpenGL::V3_2;
-    let mut window: PistonWindow = WindowSettings::new("flappy bird", (700, 350))
+    let mut window: PistonWindow = WindowSettings::new("flappy bird", (WINDIMS[0], WINDIMS[1]))
         .title("Not Flappybird".to_string())
         .graphics_api(opengl)
         .resizable(false)
@@ -29,20 +32,34 @@ fn main() {
     // event loop
 
     let mut events = Events::new(EventSettings::new().ups(60).max_fps(60));
-
-
+    let ds = graphics::DrawState::default();
+    let mut stage_offset: f64 = 0.0;
+    let mut xvel: f64 = 1.0; // the starting x velocity
     while let Some(ev) = events.next(&mut window) {
+
         if let Some(_) = ev.update_args() {}
+
         if let Some(_) = ev.render_args() {
+            // increment stage movement
+
+            stage_offset -= xvel;
+            stage_offset %= WINDIMS[1];
             // Background draw, in reality this should
             // be defined within its own module
-
-
             window.draw_2d(&ev, |c, g, _| {
                 clear([1.0; 4], g);
-                // call makeup : image itself, context mutations, graphics
-                image(&am.bg_tex, c.transform, g);
-                image(&am.bg_tex, c.transform, g);
+
+                // Background Section
+                // due to math, there are always three images
+                for image_idx in 0..3 {
+                    // prevents image seams
+                    let jitter_offset = image_idx as f64;
+                    let x_coord = (jitter_offset * 350.0) + stage_offset - jitter_offset;
+                    let j = Image::new().rect(square(x_coord, 0.0, WINDIMS[1]));
+                    // call makeup : image itself, context mutations, graphics
+                    j.draw(&am.bg_tex, &ds, c.transform, g);
+                }
+
             });
         }
 
