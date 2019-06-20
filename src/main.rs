f9c2a609774d837285907b502b5a979a743efc2a
mobile-bungalow <pwmay@ucsc.edu>
2019-06-20 15:20:41 -0700

    texture done, next transforms

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 mod bird;
 mod pipe;
 mod render_pl;
-extern crate piston;
 
+extern crate graphics;
+extern crate piston;
 // piston reqs
 use piston_window::*;
 // user input
@@ -15,9 +16,10 @@ use vecmath::Vector2;
 fn main() {
     // build window
     let opengl = OpenGL::V3_2;
-    let mut window: PistonWindow = WindowSettings::new("flappy bird", (950, 800))
+    let mut window: PistonWindow = WindowSettings::new("flappy bird", (700, 350))
         .title("Not Flappybird".to_string())
         .graphics_api(opengl)
+        .resizable(false)
         .build()
         .unwrap_or_else(|e| panic!("Window didn't build: {}", e));
 
@@ -31,9 +33,18 @@ fn main() {
 
     while let Some(ev) = events.next(&mut window) {
         if let Some(_) = ev.update_args() {}
-        window.draw_2d(&ev, |c, g, _| {
-            clear([1.0; 4], g);
-            image(&am.bg_tex, c.transform, g);
-        });
+        if let Some(_) = ev.render_args() {
+            // Background draw, in reality this should
+            // be defined within its own module
+
+
+            window.draw_2d(&ev, |c, g, _| {
+                clear([1.0; 4], g);
+                // call makeup : image itself, context mutations, graphics
+                image(&am.bg_tex, c.transform, g);
+                image(&am.bg_tex, c.transform, g);
+            });
+        }
+
     }
 }
