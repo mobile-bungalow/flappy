9118b562a7de4c38ff4756e25af367e143e27add
mobile-bungalow <pwmay@ucsc.edu>
2019-06-22 01:04:43 -0700

    spawn logic

--- src/pipe.rs
+++ src/pipe.rs
@@ -3,8 +3,8 @@ extern crate rand;
 use rand::Rng;
 
 // Max challenge for pipes
-static MINDIFF: f64 = 60.0;
-static MAXDIFF: f64 = 30.0;
+static MINDIFF: f64 = 30.0;
+static MAXDIFF: f64 = 60.0;
 
 // range for the gap
 static MAXHEIGHT: f64 = 340.0;
@@ -26,12 +26,12 @@ impl Pipe {
     /// based off of ther current speed
     /// xvel is the current challenge, start is the spawn position,
     /// spawn time is the time the pipe was born.
-    pub fn new(_xvel: f64, start: f64, spawn_time: u64) -> Self {
+    pub fn new(start: f64, spawn_time: u64) -> Self {
         let mut rng = rand::thread_rng();
         Pipe {
             spawn_time,
-            gap: rng.gen_range(MAXDIFF, MINDIFF),
-            height: rng.gen_range(MAXHEIGHT, MINHEIGHT),
+            gap: rng.gen_range(MINDIFF, MAXDIFF),
+            height: rng.gen_range(MINHEIGHT, MAXHEIGHT),
             x: start,
         }
     }
@@ -41,13 +41,25 @@ impl Pipe {
 /// Takes a list of pipes, and delta time since the
 /// game began, every N seconds after a certain time
 /// it pushes a pipe off the stack and generates a new one
-pub fn update_pipe_state(_pipe_deque: &mut Vec<Pipe>, dt: u64) {
+/// Yeah , I know this is ugly at the moment, there are better
+/// solutions
+pub fn update_pipe_state(pipe_deque: &mut Vec<Pipe>, dt: u64) {
     // latent stage
     if dt < LATENT {
         return;
     };
 
     if dt < SPAWNING {
+        if let Some(p) = pipe_deque.first() {
+            if dt - p.spawn_time > 50 {
+                // enough time has elapsed to spawn another pipe
+                // current tick, and where it should spawn off screen
+                pipe_deque.push(Pipe::new(850.0, dt));
+            }
+        } else {
+            // current tick, and where it should spawn off screen
+            pipe_deque.push(Pipe::new(850.0, dt));
+        }
         return;
     };
 
