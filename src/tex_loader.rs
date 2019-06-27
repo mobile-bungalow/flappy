ccbcad8597c3fffe6c90692e17aeb0777a56e102
mobile-bungalow <pwmay@ucsc.edu>
2019-06-27 12:21:12 -0700

    include files in binary

--- src/tex_loader.rs
+++ src/tex_loader.rs
@@ -1,14 +1,18 @@
-use gfx_graphics::*;
+extern crate image;
 
-static TEXLIST: [&str; 7] = [
+use gfx_graphics::*;
+use image::DynamicImage::ImageRgba8;
+
+
+static TEXLIST: [&[u8]; 7] = [
     //new background that does not contain ground
-    "src/assets/bg_new.png",
-    "src/assets/bird.png",
-    "src/assets/pipe.png",
-    "src/assets/bird_up.png",
-    "src/assets/gameover.png",
-    "src/assets/message.png",
-    "src/assets/base.png",
+    include_bytes!("assets/bg_new.png"),
+    include_bytes!("assets/bird.png"),
+    include_bytes!("assets/pipe.png"),
+    include_bytes!("assets/bird_up.png"),
+    include_bytes!("assets/gameover.png"),
+    include_bytes!("assets/message.png"),
+    include_bytes!("assets/base.png"),
 ];
 
 /// The map of all textures that OPengl needs in order to
@@ -32,12 +36,20 @@ impl AssetMap {
             gfx_device_gl::CommandBuffer,
         >,
     ) -> Self {
+
         let tex_list: Vec<Texture<gfx_device_gl::Resources>> = TEXLIST
             .iter()
-            .map(move |path| {
-                Texture::from_path(ctx, path, Flip::None, &TextureSettings::new()).unwrap()
+            .map(move |img| {
+                let img = image::load_from_memory(img);
+                let img = match img {
+                    Ok(image::DynamicImage::ImageRgba8(img)) => img,
+                    Ok(img) => img.to_rgba(),
+                    Err(e) => panic!("{}", e),
+                };
+                Texture::from_image(ctx, &img, &TextureSettings::new()).unwrap()
             })
             .collect();
+
 
         AssetMap {
             bg_tex: tex_list[0].clone(),
